8d564dcc195ba452d25ab74684ea106fd9445418
TBSten <programmingcafeteria@gmail.com>
2023-12-06 14:46:12 +0900

    to_image を実装

--- image.py
+++ image.py
@@ -1,7 +1,27 @@
 
 
+from context import DrawContext
 from elements import Element
-
+from node import RootDrawNode
+from PIL import Image
 
 def to_image(root_element: Element):
-    pass
+    # layout
+    context = DrawContext(Image.new("RGBA", (0, 0)))
+    el_draw_node = RootDrawNode(node_id="root-node", root_node=root_element.layout(context))
+    el_draw_node.x = 0
+    el_draw_node.y = 0
+
+    w = el_draw_node.w
+    h = el_draw_node.h
+
+    if w is None or h is None:
+        raise NotImplementedError(f"invalid root element size {w}*{h}")
+
+    # render
+    context = DrawContext(Image.new("RGBA", (int(w), int(h))))
+    el_draw_node.draw(context)
+    image = context.img
+
+    return image
+
