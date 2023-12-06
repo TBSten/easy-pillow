14c4e0fb89ad953e69e1fe0ae365e86dc6aec950
TBSten <81161390+TBSten@users.noreply.github.com>
2023-12-07 01:51:07 +0900

    デバッグ用ユーティリティを追加

---
+++ core/debug.py
@@ -0,0 +1,11 @@
+from core.node import DrawNode
+
+
+def print_node_tree(root_node: DrawNode):
+    _print_node(root_node, 0)
+
+
+def _print_node(node: DrawNode, indent: int):
+    print(f"{'  '*indent}{node.label}")
+    for child in node.children:
+        _print_node(child, indent+1)
