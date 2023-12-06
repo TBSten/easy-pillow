7633c112533626b1e32803135f0af6d23127d2ed
TBSten <81161390+TBSten@users.noreply.github.com>
2023-12-07 01:54:34 +0900

    sampleを整理

--- sample.py
+++ sample.py
@@ -1,39 +1,66 @@
 
-from attributes.padding import PaddingAttribute
+import sys
+
+from attributes.background import BackgroundAttribute
+from attributes.border import BorderAttribute
+from attributes.size import WidthAttribute, size
+from core.align import HorizontalAlign
+from core.color import Color
+from core.elements import Element
 from core.image import to_image
 from elements.column import ColumnElement
 from elements.row import RowElement
 from elements.spacer import Spacer
 
-image = to_image(
-    ColumnElement(
-        children=[
-            ColumnElement(
-                children=[
-                    Spacer(width=100, height=100, color=(255, 0, 0, 128)),
-                ],
-            ),
-            ColumnElement(
-                children=[
-                    Spacer(width=200, height=200, color=(0, 255, 0, 128)),
-                    Spacer(width=200, height=200, color=(0, 255, 0, 128+64)),
-                    Spacer(width=200, height=200, color=(0, 255, 0, 255)),
-                ],
-            ),
-            Spacer(width=300, height=300, color=(0, 0, 255, 128)),
-            RowElement(
-                attrs=[PaddingAttribute(top=100, bottom=100)],
-                children=[
-                    Spacer(width=100, height=100, color=(0, 0, 0)),
-                    Spacer(width=100, height=200, color=(0, 0, 0, 128)),
-                    Spacer(width=100, height=100, color=(0, 0, 0)),
-                    Spacer(width=100, height=200, color=(0, 0, 0, 128)),
-                    Spacer(width=100, height=100, color=(0, 0, 0)),
-                    Spacer(width=100, height=200, color=(0, 0, 0, 128)),
-                    Spacer(width=100, height=100, color=(0, 0, 0)),
-                ],
-            ),
-        ],
-    ),
-)
-image.save("./output.png")
+GREEN = Color(0, 255, 0)
+RED = Color(255, 0, 0, 128)
+BLUE = Color(0, 0, 255)
+BLACK = Color.BLACK
+WHITE = Color.WHITE
+
+
+def test1():
+    spacers: list[Element] = [
+        Spacer(attrs=[
+            BackgroundAttribute(RED.copy(a=255*0.5)),
+            size(30, 30),
+        ]),
+        Spacer(attrs=[
+            BackgroundAttribute(RED.copy(a=255*0.25)),
+            size(30, 30),
+        ]),
+        Spacer(attrs=[
+            BackgroundAttribute(RED.copy(a=255*0.125)),
+            size(30, 30),
+        ]),
+    ]
+    row_sample = RowElement(
+        horizontal_gap=10,
+        horizontal_align=HorizontalAlign.CENTER,
+        attrs=[BorderAttribute(45, WHITE), WidthAttribute(50)],
+        children=spacers,
+    )
+    col_sample = ColumnElement(
+        vertical_gap=10,
+        attrs=[BorderAttribute(1, WHITE)],
+        children=spacers,
+    )
+    img = to_image(
+        ColumnElement(
+            vertical_gap=10,
+            children=[
+                RowElement(
+                    children=[row_sample, col_sample],
+                    horizontal_gap=10,
+                ),
+                RowElement(
+                    children=[col_sample, row_sample],
+                    horizontal_gap=10,
+                ),
+            ],
+        )
+    )
+    img.save("./output/test1.png")
+
+
+test1()
