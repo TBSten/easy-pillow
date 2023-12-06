fc45f9afc315aa1d89f1e73925560a2692bf5fa9
TBSten <programmingcafeteria@gmail.com>
2023-12-06 14:46:40 +0900

    サンプルを追加

---
+++ sample.py
@@ -0,0 +1,37 @@
+
+from image import to_image
+from attributes import PaddingAttribute
+from elements import ColumnElement, RowElement, Spacer
+
+image = to_image(
+    ColumnElement(
+        children=[
+            ColumnElement(
+                children=[
+                    Spacer(width=100, height=100, color=(255, 0, 0, 128)),
+                ],
+            ),
+            ColumnElement(
+                children=[
+                    Spacer(width=200, height=200, color=(0, 255, 0, 128)),
+                    Spacer(width=200, height=200, color=(0, 255, 0, 128+64)),
+                    Spacer(width=200, height=200, color=(0, 255, 0, 255)),
+                ],
+            ),
+            Spacer(width=300, height=300, color=(0, 0, 255, 128)),
+            RowElement(
+                attrs=[PaddingAttribute(top=100, bottom=100)],
+                children=[
+                    Spacer(width=100, height=100, color=(0, 0, 0)),
+                    Spacer(width=100, height=200, color=(0, 0, 0, 128)),
+                    Spacer(width=100, height=100, color=(0, 0, 0)),
+                    Spacer(width=100, height=200, color=(0, 0, 0, 128)),
+                    Spacer(width=100, height=100, color=(0, 0, 0)),
+                    Spacer(width=100, height=200, color=(0, 0, 0, 128)),
+                    Spacer(width=100, height=100, color=(0, 0, 0)),
+                ],
+            ),
+        ],
+    ),
+)
+image.save("./output.png")
