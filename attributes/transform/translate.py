064baac6f9cfe10d1fae366a6051039d0b732658
TBSten <programmingcafeteria@gmail.com>
2023-12-11 15:27:39 +0900

    translateを追加

---
+++ attributes/transform/translate.py
@@ -0,0 +1,51 @@
+from core.attributes import Attribute
+from core.context import DrawContext
+from core.layout import LayoutConstraints
+from core.node import DrawNode, DrawNodeType, Parent
+from core.unit import Length
+
+from PIL import Image
+
+class TranslateAttribute(Attribute):
+  def __init__(self, x :Length=0, y :Length=0):
+    super().__init__()
+    self.translate_x = x
+    self.translate_y = y
+
+  def on_layout(self, context: DrawContext, constraints: LayoutConstraints, target: DrawNode) -> DrawNode:
+    translate_node = TranslateAttribute.Node(
+      label="translate-attr",
+      x=target.x,
+      y=target.x,
+      w=target.w,
+      h=target.h,
+      children=[target],
+      translate_x=self.translate_x,
+      translate_y=self.translate_y,
+    )
+    target.parent = translate_node
+    target.x = self.translate_x
+    target.y = self.translate_y
+    return translate_node
+
+  class Node(DrawNode):
+    def __init__(
+      self, 
+      label, 
+      translate_x :Length, translate_y :Length,
+      parent: Parent = None, 
+      x: Length = None, y: Length = None, 
+      w: Length = None, h: Length = None, 
+      children: list[DrawNodeType] = [],
+    ) -> None:
+      super().__init__(label, parent, x, y, w, h, children)
+      self.translate_x = translate_x
+      self.translate_y = translate_y
+
+    def on_draw(self, context: DrawContext):
+      base = context.img
+      context.img = Image.new("RGBA", context.img.size)
+      self.draw_children(context)
+      base.alpha_composite(context.img, dest=(int(self.translate_x), int(self.translate_y)))
+      context.img = base
+
